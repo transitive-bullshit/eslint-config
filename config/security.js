c8197c559936551b743da358af4b91496c49031a
Travis Fischer <fisch0920@gmail.com>
2024-04-07 13:08:55 -0500

    feat: update deps; disable some overly strict security rules

--- config/security.js
+++ config/security.js
@@ -2,5 +2,10 @@
 module.exports = {
   plugins: ['security'],
 
-  extends: ['plugin:security/recommended-legacy']
+  extends: ['plugin:security/recommended-legacy'],
+
+  rules: {
+    'security/detect-non-literal-fs-filename': 'off',
+    'security/detect-object-injection': 'off'
+  }
 }
